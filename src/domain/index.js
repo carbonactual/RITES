4ff05756f99369235a6bbee2a5839677c0f633d6
carbonactual <carbonactuall@gmail.com>
2026-08-19 02:10:06 +0100

    feat(domain): export authority and event runtime modules

--- src/domain/index.js
+++ src/domain/index.js
@@ -2,3 +2,5 @@ export * from './continuum.js';
 export * from './digitalEstate.js';
 export * from './scenarios.js';
 export * from './reporting.js';
+export * from './authority.js';
+export * from './events.js';
